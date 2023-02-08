78a500dc35aab1c2daef6f98f2491078377b6573
Divianshu7 <120759278+Divianshu7@users.noreply.github.com>
2023-02-09 04:49:32 +0530

    Update index.js
    update removal of react dev tools

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StateProvider } from './StateProvider';
 import reducer, { initialState } from './reducer';
-const root = ReactDOM.createRoot(document.getElementById('root'));
 if (process.env.NODE_ENV === 'production') {
   disableReactDevTools();
 }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
 
   <StateProvider initialState={initialState} reducer={reducer}>
